ce97b0216a7b73960f9c1666a78ddf3d5470f88c
Adam Kaluza <adam.kaluza.97@gmail.com>
2021-05-24 23:37:47 +0200

    add button component

---
+++ src/app/button/button.component.ts
@@ -0,0 +1,13 @@
+import {EventEmitter, Input} from '@angular/core';
+import {Component, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent {
+
+  @Output() clickButton  = new EventEmitter<void>();
+  @Input() customClasses: string | undefined;
+}
