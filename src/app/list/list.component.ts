98c5867086a480eb348a9fef378a2698b4ffc88f
Adam Kaluza <adam.kaluza.97@gmail.com>
2021-05-25 22:05:31 +0200

    add list component

---
+++ src/app/list/list.component.ts
@@ -0,0 +1,24 @@
+import { EventEmitter } from '@angular/core';
+import {Component, Input, Output} from '@angular/core';
+import {Quotation} from "../Models/quotation";
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent  {
+
+  @Input() quotes: Quotation[] | undefined;
+  @Output() vote = new EventEmitter<QuotationEvent>();
+
+  addVote( quotation: Quotation, value: number){
+    this.vote.emit({quotation,value});
+  }
+}
+
+
+export interface QuotationEvent {
+  quotation: Quotation;
+  value: number;
+}
